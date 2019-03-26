dc763d7b1a95a74c6d109a03e34ba45315212195
Hans Verkuil <hverkuil-cisco@xs4all.nl>
2019-03-26 08:38:55 +0100

    Update email addresses
    I switched to hverkuil-cisco@xs4all.nl.
    
    Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>

--- edid-decode.c
+++ edid-decode.c
@@ -20,7 +20,7 @@
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 /* Author: Adam Jackson <ajax@nwnk.net> */
-/* Maintainer: Hans Verkuil <hans.verkuil@cisco.com> */
+/* Maintainer: Hans Verkuil <hverkuil-cisco@xs4all.nl> */
 
 #include <sys/types.h>
 #include <sys/stat.h>
